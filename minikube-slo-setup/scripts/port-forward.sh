9b64478c1c3d7006d125250a889b1f14ba0d60d4
surendra <31211689+surendranaidu@users.noreply.github.com>
2024-11-21 19:18:48 +0530

    Delete minikube-slo-setup directory

--- minikube-slo-setup/scripts/port-forward.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Forwarding Prometheus and Grafana..."
-kubectl port-forward svc/prometheus-kube-prometheus-prometheus -n monitoring 9090:9090 &
-kubectl port-forward svc/prometheus-grafana -n monitoring 3000:80 &
-echo "Prometheus: http://localhost:9090"
-echo "Grafana: http://localhost:3000"
--- minikube-slo-setup/scripts/start-minikube.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting Minikube..."
-minikube start --memory=4096 --cpus=2
-minikube addons enable ingress
-minikube addons enable metrics-server
