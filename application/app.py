001a91a053bf110c37b6a1d5a6de98ed8ded214b
surendra <31211689+surendranaidu@users.noreply.github.com>
2024-11-24 15:54:21 +0530

    Update app.py

--- application/app.py
+++ application/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World! surendra k8 dev test"
+    return "Hello, World! surendra k8 dev test 2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
